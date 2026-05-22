bb3e19188f51d9afdfc4903ce6f94f0dee4665d0
hekaa887 <nikitossidorovich@gmail.com>
2026-05-22 22:17:00 +0300

    feat: create models for user reviews and favorites

---
+++ server/src/models/Review.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Review = sequelize.define(
+  'Review',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'reviews',
+    timestamps: true,
+  }
+);
+
+module.exports = Review;
